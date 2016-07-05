e767d45efb73ba5c03842bfef2b1eadb0faad760
hulujie <741802012@qq.com>
2016-07-05 11:20:26 +0800

    user

---
+++ src/com/wizard/main/User.java
@@ -0,0 +1,17 @@
+package com.wizard.main;
+
+public class User {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
