871d78a9e50e4abbd269b381ab80893a6f62dff7
hulujie <741802012@qq.com>
2016-07-05 11:12:56 +0800

    qwe

--- src/com/wizard/main/Main.java
+++ src/com/wizard/main/Main.java
@@ -1,7 +1,11 @@
 package com.wizard.main;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 	public static void main(String[] args) {
 		System.out.println(123456);
+		List<String> list = new ArrayList<String>();
 	}
 }
